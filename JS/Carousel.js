49eeec416e443fc7cafc9b55dbfdf1c828413611
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-06-25 16:01:23 -0500

    Added JS file and completed carousel for images

---
+++ JS/Carousel.js
@@ -0,0 +1,43 @@
+class Carousel {
+    constructor(element){
+        this.element = element;
+        this.leftBtn = document.querySelector('.carousel .left-button');
+        this.rightBtn = document.querySelector('.carousel .right-button');
+        this.index = 0;
+        this.image = element.querySelectorAll('img')[this.index];
+        this.image.style.display = "block";
+        this.images = element.querySelectorAll(".carousel img");
+        this.rightBtn.addEventListener('click', () => {
+            this.clickRight();
+        });
+        this.leftBtn.addEventListener('click', () => {
+            this.clickLeft();
+        });
+    }
+    summonImages(){
+        this.images.forEach(item => {
+            item.style.display = "none";
+        });
+    }
+    displayImages(){
+        this.images[this.index].style.display = "block";
+    }
+    clickRight(){
+        if (this.index < Array.from(this.images).length - 1){
+            this.index++;
+            this.summonImages();
+            this.displayImages();
+        }
+    }
+  
+    clickLeft(){
+        if (this.index > 0){
+            this.index--;
+            this.summonImages();
+            this.displayImages();
+        }
+    }
+}
+
+let carousel = document.querySelectorAll(".carousel");
+carousel.forEach(element => new Carousel(element));
